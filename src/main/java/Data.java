7f9bf8bfddc8e9c9069b4f4a48ffea24563637f2
Anna <anafbtest5@gmail.com>
2019-07-12 10:37:21 -0400

    First steps for creating POM model
    Add 3 test classes. 1 Base - parent test class. Create Data and Locators classes.

---
+++ src/main/java/Data.java
@@ -0,0 +1,9 @@
+public class Data {
+     //Registration data
+    public static final String email = "test@gmail.com";
+    public static final String password = "566767m";
+    public static final String nickname = "test566767m";
+    public static final String phone = "6546478754";
+
+    public static final  String expectedUrlMedia = "https://romanceabroad.com/media/index";
+}
---
+++ src/test/java/SearchTests.java
@@ -0,0 +1,20 @@
+import org.openqa.selenium.By;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class SearchTests extends BaseUI {
+
+    String currentUrlSearch;
+    String expectedUrlSearch = "https://romanceabroad.com/users/search";
+
+
+    @Test
+    public void testSearchPage () {
+
+        driver.findElement(Locators.LINK_SEARCH).click();
+        currentUrlSearch = driver.getCurrentUrl();
+        System.out.println(currentUrlSearch);
+        Assert.assertEquals(currentUrlSearch, expectedUrlSearch);
+
+    }
+}
